278f71b2aa7aa7257240d031bd0f5becf63b142d
litonbairaggi <litonovi2013@gmail.com>
2021-09-22 09:52:14 +0600

    Repositorie list design

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -7,32 +7,33 @@ const Repo = ({ data, repositories }) => {
             <div className="row">
                 <div className="col-md-12">
 
-                <div className="row">
-                    
-                    <div className="col-md-12">
-                    <hr/>
-                    {repositories.map(repo => (
-                        <div className="ui relaxed divided list" key={repo.name}>
-                            <div className="item">
-                                <i className="large github middle aligned icon"></i>
-                                <div className="content">                                    
-                                    <div className="listitems">
-                                    <span><a href={repo.html_url} className="header" target="_blank" rel="noreferrer">
-                                        {repo.name}</a></span><br></br>
-                                        <span className="label label-default">{repo.watchers} Watchers</span>
-                                        <span className="label label-primary">{repo.forks} Forks</span>
-                                        
+                    <div className="row">
+                        
+                        <div className="col-md-12">
+                        <hr/>
+                        {repositories.map(repo => (
+                            <div className="ui relaxed divided list" key={repo.name}>
+                                <div className="item">
+                                    <i className="large github middle aligned icon"></i>
+                                    <div className="content"> 
+                                        <div className="row">
+                                            <div className="col-md-6">                                   
+                                                <span><a href={repo.html_url} className="header" target="_blank" rel="noreferrer">
+                                                    {repo.name}</a></span><br></br>
+                                            </div>
+                                            <div className="col-md-6">
+                                                <span className="label label-default">{repo.watchers} Watchers</span>
+                                                <span className="label label-primary">{repo.forks} Forks</span>
+                                            </div> 
+                                        </div>
                                     </div>
                                 </div>
+                                <hr/>
                             </div>
-                            <hr/>
+                        ))}
+
                         </div>
-                    ))}
-
                     </div>
-                </div>
-
-
                 </div>
             </div>
         </div>
