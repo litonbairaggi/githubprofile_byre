f49086c9c5860c2b054dc230914f471bd0d6b609
litonbairaggi <litonovi2013@gmail.com>
2021-09-22 10:32:20 +0600

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../images/Octocat.png';
 import './Header.css'
 const Header = () => {
     return (
-
+        
             <header>
                 <div className="header-container profileimage">
                     <img src={logo} alt="Github"/>
